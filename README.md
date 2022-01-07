645f6bdcc93e0f27aace4c20344e11ea140711d8
Mustafa Ibrahim <mustafa.m.kannan@gmail.com>
2022-01-07 09:56:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project-tracker
+ Monitoring tool to track software projects development
---
+++ src/test/java/com/interfaceict/ptrakcer/ProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interfaceict.ptrakcer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
