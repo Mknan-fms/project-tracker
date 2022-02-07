5da187f624063556ac83bf7bcaa0b26fb15f7e7b
Htrap19 <developmentprogramming154@gmail.com>
2022-02-07 13:13:41 +0200

    Added missing ConstraintViolationException handler

--- src/main/java/com/interfaceict/ptrakcer/controllers/advices/GlobalControllerHelper.java
+++ src/main/java/com/interfaceict/ptrakcer/controllers/advices/GlobalControllerHelper.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class GlobalControllerHelper extends ResponseEntityExceptionHandler
@@ -17,5 +18,12 @@ public class GlobalControllerHelper extends ResponseEntityExceptionHandler
     public ResponseEntity<Object> handleNotFoundException(NotFoundException notFoundException, WebRequest request)
     {
         return new ResponseEntity<>(notFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request)
+    {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
